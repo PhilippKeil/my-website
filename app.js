6b9b7b8002ffb9bbcab1a1d9e5e5b7c1eb2e96ff
Philipp Keil <philippkeil95@gmail.com>
2016-09-07 23:37:54 +0200

    simplified routing for now

--- app.js
+++ app.js
@@ -2,12 +2,28 @@ var express = require('express');
 var app = express();
 var path = require('path');
 
-var routes = require(path.join(__dirname, 'routes'));
-
 app.set('port', 8080)
+app.set('views', path.join(__dirname, 'views'))
 
-app.get('/', routes.index);
-app.get('/about', routes.about);
+// ROUTING
+
+//root
+app.get('/', function(req, res) {
+  res.sendFile(path.join(app.get('views'), 'index.html'));
+  console.log('New GET req to /');
+});
+
+//about
+app.get('/about', function(req, res) {
+  res.send('About Page');
+  console.log('GET request to /about')
+});
+
+//404
+app.get('*', function(req, res) {
+  res.send('LUL WHERE YOU AT BRAH?');
+  console.log('GET req to 404')
+});
 
 app.listen(app.get('port'), function () {
   console.log('Example app listening on port ' + app.get('port') + '!');
--- routes/index.js
+++
@@ -1,9 +0,0 @@
-exports.index = function(req, res) {
-  res.send('Hello World');
-  console.log('GET req on /');
-};
-
-exports.about = function(req, res) {
-  res.send('About page');
-  console.log('GET req on /about');
-};
