15d292bb60f6b6666fb5a6933dc2e37fafa472db
Philipp Keil <philippkeil95@gmail.com>
2016-09-08 06:19:33 +0200

    added mouse hover coloring

--- public/js/news.js
+++ public/js/news.js
@@ -4,9 +4,15 @@ var main = function() {
     //Click on a article title reveals description or hides it if its already open
     if ($(this).next('.article-description').css('display') == 'none') {
       //description is not already open
+
+      //remove .hover early
+      $(this).removeClass('hover');
+
+      //make it the only active article
       $('.article-title').removeClass('active-article');
       $(this).addClass('active-article');
 
+      //make it the only visible description
       $('.article-description').hide();
       $(this).next('.article-description').show();
     }
@@ -14,6 +20,16 @@ var main = function() {
       $('.article-title').removeClass('active-article');
       $('.article-description').hide();
     };
+  });
+  $('.article-title').hover(function() {
+    //mouse goes IN
+    if ($(this).hasClass('active-article') == false) {
+      $(this).addClass('hover');
+      console.log('hover event');
+    }
+  }, function() {
+    //mouse goes OUT
+    $(this).removeClass('hover');
   });
 };
 
