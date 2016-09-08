4a8ed0ce9557485d15f9dd420465bca0d82d8247
Philipp Keil <philippkeil95@gmail.com>
2016-09-08 05:46:49 +0200

    added color feedback for active article

--- public/js/news.js
+++ public/js/news.js
@@ -4,10 +4,14 @@ var main = function() {
     //Click on a article title reveals description or hides it if its already open
     if ($(this).next('.article-description').css('display') == 'none') {
       //description is not already open
+      $('.article-title').removeClass('active-article');
+      $(this).addClass('active-article');
+
       $('.article-description').hide();
       $(this).next('.article-description').show();
     }
     else {
+      $('.article-title').removeClass('active-article');
       $('.article-description').hide();
     };
   });
