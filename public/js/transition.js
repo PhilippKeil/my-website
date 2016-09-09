71114de595ce8a997a822766449330580586aea1
Philipp Keil <philippkeil95@gmail.com>
2016-09-09 09:26:18 +0200

    testing animation concepts

--- public/js/transition.js
+++ public/js/transition.js
@@ -1,13 +1,19 @@
 var config = {};
-config.window = $(window);
+config.window = $(document);
 config.wWidth = config.window.width();
 config.wHeight = config.window.height();
 
-config.t_lft_rght = 0.55;
-config.t_top_btm = 0.55;
-config.t_start_top_btm = 0.275;
-config.t_wait_left_rght = 0.275;
-config.rotation = 20;
+config.t_lft_rght = 5.5; // 0.55
+config.t_top_btm = 5.5; // 0.55
+config.t_start_top_btm = 2.75; // 0.275
+config.t_wait_left_rght = 2.75; // 0.275
+config.rotation = 0;
+
+// config.rotation_offset = Math.sqrt(Math.pow(config.wWidth/2, 2) + Math.pow(config.wHeight/2, 2)) - config.wWidth/2;
+config.rotation_offset = 0;
+console.log('width ' + config.wWidth);
+console.log('height ' + config.wHeight);
+console.log(config.rotation_offset);
 
 config.pageTrans = new TimelineMax({repeat:-1, repeatDelay: 1, yoyo:false});
 
@@ -26,10 +32,10 @@ config.pageTrans
 .fromTo('#pagetransition', (config.t_start_top_btm + config.t_top_btm), { rotation: 0 }, { rotation: config.rotation, ease: Power2.easeOut}, "f")
 
 // panel OPEN
-.fromTo('.btm-left',config.t_lft_rght, { x: 0 }, { x: -config.wWidth/2, ease: Power2.easeIn}, "f+=" + (config.t_start_top_btm + config.t_top_btm + config.t_wait_left_rght))
-.fromTo('.top-right',config.t_lft_rght, { x: 0 }, { x: config.wWidth/2, ease: Power2.easeIn}, "f+="+ (config.t_start_top_btm + config.t_top_btm + config.t_wait_left_rght))
-.fromTo('.top-left',config.t_lft_rght, { y: 0 }, { y: -config.wHeight, ease: Power2.easeIn}, "f+="+ (config.t_start_top_btm + config.t_top_btm + config.t_wait_left_rght))
-.fromTo('.btm-right',config.t_lft_rght, { y: 0 }, { y: config.wHeight, ease: Power2.easeIn}, "f+="+ (config.t_start_top_btm + config.t_top_btm + config.t_wait_left_rght))
+.fromTo('.btm-left',config.t_lft_rght, { x: 0 }, { x: -config.wWidth/2 - config.rotation_offset, ease: Power2.easeIn}, "f+=" + (config.t_start_top_btm + config.t_top_btm + config.t_wait_left_rght))
+.fromTo('.top-right',config.t_lft_rght, { x: 0 }, { x: config.wWidth/2 + config.rotation_offset, ease: Power2.easeIn}, "f+="+ (config.t_start_top_btm + config.t_top_btm + config.t_wait_left_rght))
+.fromTo('.top-left',config.t_lft_rght, { y: 0 }, { y: -config.wHeight + config.rotation_offset, ease: Power2.easeIn}, "f+="+ (config.t_start_top_btm + config.t_top_btm + config.t_wait_left_rght))
+.fromTo('.btm-right',config.t_lft_rght, { y: 0 }, { y: config.wHeight - config.rotation_offset, ease: Power2.easeIn}, "f+="+ (config.t_start_top_btm + config.t_top_btm + config.t_wait_left_rght))
 // logo
 .fromTo('.brand-logo', config.t_lft_rght, { rotation: 0, scale: 0 }, { rotation: -config.rotation, scale: 1, ease: Back.easeOut}, "f")
 .fromTo('.brand-logo', config.t_lft_rght, { rotation: -config.rotation, scale: 1 }, { rotation: 0, scale: 0, ease: Back.easeIn}, "f+=" + (config.t_start_top_btm + config.t_top_btm))
