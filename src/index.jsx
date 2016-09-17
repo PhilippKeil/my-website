4277a25a742699c74538f5ea2c249aee485d6963
Philipp Keil <philippkeil95@gmail.com>
2016-09-17 16:00:57 +0200

    formatting + added /about

--- src/index.jsx
+++ src/index.jsx
@@ -9,13 +9,14 @@ import {Router, Route, browserHistory} from 'react-router';
 import Main from './common/main.component.jsx';
 import Home from './common/home.component.jsx';
 import News from './common/news.component.jsx';
+import About from './common/about.component.jsx';
 
 render(
   <Router history={browserHistory}>
     <Route component={Main}>
       <Route path="/" component={Home}/>
       <Route path="/news" component={News}/>
-      <Route path="/portfolio"/>
+      <Route path="/About" component={About}/>
       <Route path="/contact"/>
     </Route>
   </Router>,
